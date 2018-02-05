2799c28728db8f9577a62054b5ae8208af08e67e
Radoslav Gatev <radoslavgatev@gmail.com>
2018-02-05 09:35:30 +0200

    Updated the description of the VSIX.

--- src/SSDT-CD-VSIX/Properties/AssemblyInfo.cs
+++ src/SSDT-CD-VSIX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SSDT_CI_VSIX")]
+[assembly: AssemblyTitle("SSDT_CD_VSIX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SSDT_CI_VSIX")]
+[assembly: AssemblyProduct("SSDT_CD_VSIX")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
